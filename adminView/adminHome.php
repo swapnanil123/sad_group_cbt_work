92b02825bfaf3f83601ca17d051037dad86c6206
akash031 <diwanakash15@yahoo.com>
2021-07-19 19:45:00 +0530

    examdetails

--- adminView/adminHome.php
+++ adminView/adminHome.php
@@ -123,7 +123,7 @@
                                 <div class="card bg-primary text-white mb-4">
                                     <div class="card-body">Exam Entry</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#"><!--text--></a>
+                                        <a class="small text-white stretched-link" href="examDetails.html"><!--text--></a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
