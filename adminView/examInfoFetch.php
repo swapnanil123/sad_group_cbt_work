88dc8f1460f03c7395909a1c30fbd4bf6e8f1d42
swapnanil123 <nilswapnanil10@gmail.com>
2021-07-26 00:45:25 +0530

    update

--- adminView/examInfoFetch.php
+++ adminView/examInfoFetch.php
@@ -25,11 +25,11 @@ if ( $allQuestions > 0) {
             
             <td>
                 <input type="button" class="btn btn-success btn-sm shadow-none" id="startBtn_<?php echo $result['exam_serial_no'] ?>" onclick="getData(this.id)" value="Start">
-                <input type="button" id="stopBtn_<?php echo $result['exam_serial_no'] ?>" onclick="stopBtnId(this.id)" class="btn btn-secondary btn-sm shadow-none" value="Stop">
+                <input type="button" id="stopBtn_<?php echo $result['exam_serial_no'] ?>" onclick="stopBtnId(this.id)" class="btn btn-secondary btn-sm shadow-none" disabled value="Stop">
                 <input type="button" id="delBtn_<?php echo $result['exam_serial_no'] ?>" onclick="getDelBtnId(this.id)" class="btn btn-danger btn-sm shadow-none" value="Delete">
             </td>
 
-            <td class= "examCodeID" > <?php echo $result['exam_serial_no'] ?> </td>
+            <td class= "examCodeID" hidden > <?php echo $result['exam_serial_no'] ?> </td>
             
         </tr>
         
