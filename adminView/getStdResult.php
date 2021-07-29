eda9f12a38dccf1253262b935e3030001d50daba
swapnanil123 <nilswapnanil10@gmail.com>
2021-07-29 13:14:16 +0530

    Khela hobe

--- adminView/getStdResult.php
+++ adminView/getStdResult.php
@@ -8,39 +8,74 @@ $examCode =$_POST['examCode'];
 $qrr = 'SELECT * FROM questiondetails WHERE exam_code = "'.$examCode.'" ' ;
 
 $sql = mysqli_query($con, $qrr);
-$totalQtn = mysqli_num_rows($sql);
-echo $totalQtn;
+$totalMakrs = mysqli_num_rows($sql);
+// echo $totalMakrs; 
 
 
+$q = 'SELECT * FROM studentlogindata WHERE exam_serial_no = "'.$examCode.'" ';
+$query = mysqli_query($con, $q);
+$totalstd = mysqli_num_rows($query);
+;
+for ($i=1; $i<=$totalstd; $i++) {
+    while($stdData = mysqli_fetch_array($query)) {
+        $stdRoll = $stdData['std_roll'];
+        $count = 0;
 
-// $q = 'SELECT * FROM studentlogindata WHERE exam_serial_no = "'.$examCode.'" ' ;
+        $stdRes = 'SELECT * FROM examanswer WHERE stdRoll = "'.$stdRoll.'" and exam_code = "'.$examCode.'" ';
+        $Quer = mysqli_query($con, $stdRes);
+        $stdResDetails = mysqli_num_rows($Quer);
+        for($j=1; $j<=$stdResDetails; $j++) {
+            while($stdRD = mysqli_fetch_array($Quer)) {
+                $givenAns = $stdRD['stdGivenAnswer'];
+                $rightAns = $stdRD['currectAnswer'];
 
-// $query = mysqli_query($con, $q);
-// $allQuestions = mysqli_num_rows($query);
-// $result = mysqli_fetch_array($query);
+                
 
-// $stdRoll = $result[3];
-// // echo $allQuestions;
+                if ($givenAns == $rightAns) {
+                    $count += 1;
+                }else {
+                    $count += 0;
+                }
 
-// if ($allQuestions > 0) {
-//     for($i=0; $i< $allQuestions; $i++) {
+                break;
+
+            }
+        }
+        
+        echo $count;
+
+        $stdQ = 'SELECT * FROM studentpersonalinfo WHERE roll = "'.$stdRoll.'" ';
+        $sq = mysqli_query($con, $stdQ);
+        $stdDetails = mysqli_num_rows($sq);
+        while($stdDet = mysqli_fetch_array($sq)) {
+            $stdName = $stdDet['stdName'];
+            $stdDepartment = $stdDet['dept'];
+            $stdSemester = $stdDet['semester'];
+
+?>
+
+            <tr id="trID_">
+
+
+                <td id= "tdExamCode_ "> <?php echo $i?> </td>
+                <td id= "tdSub_"> <?php echo $stdName ?> </td>
+                <td id= "tdCourse_"> <?php echo $stdRoll ?> </td>
+                <td id= "tdTotQtn_"> <?php echo $stdDepartment ?> </td>
+                <td id= "tdTotQtn_"> <?php echo $stdSemester ?> </td>
+                <td id= "tdTotQtn_"> <?php echo $totalMakrs ?> </td>
+                <td id= "tdTotQtn_"> <?php echo $count ?> </td>
+            
+            
+            </tr>
+
+
+<?php
+        }
         
 
-//         $exD = "select * from examanswer where stdRoll = '".$stdRoll."' ";
-//         $run = mysqli_query($con, $exD);
-//         $totaldata = mysqli_num_rows($run);
-//         $ExdRes = mysqli_fetch_array($run);
-//         $count = 0;
-//         if ($ExdRes[6] == $ExdRes[7]) {
-//             $count += 1;
-//         }else {
-//             $count += 0;
-//         }
-//         // echo $count;
-      
-//     }
-// }else {
-//     console.log("Not Found");
-// }
+        break;
+    }
+} 
 
-// ?>
+
+?>
